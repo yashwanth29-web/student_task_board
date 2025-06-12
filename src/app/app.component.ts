828583e278da4ab6a6ded1c17515b80b61ce43b1
one-way binding <yashwanthbevara0@gmail.com>
2025-06-12 10:32:21 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -7,15 +7,21 @@ import { CommonModule } from '@angular/common';
   selector: 'app-root',
   imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'firstprogram';
-  userName: string = 'Yashwant devara';
-  userAge: number = 17;
-  isOnLine: boolean = true;
-  profilePic: string = 'https://avatars.githubusercontent.com/u/123456789?v=4';
-  toggleOnlineStatus() {
-    this.isOnLine = !this.isOnLine; 
-}
+  title = "FirstProgram";
+  email: string = "";
+  subject: string = "";
+  scoreOne: number = 0; 
+  scoreSecond: number = 0; 
+  statement: boolean = false;
+
+  onclick(value: string, value2: string, value3: number, value4: number) {
+    this.email = value;
+    this.subject = value2;
+    this.scoreOne = value3;
+    this.scoreSecond = value4;
+    this.statement= !this.statement  // <-- Add this line 
+  }
 }
