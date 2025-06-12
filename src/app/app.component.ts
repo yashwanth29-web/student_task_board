d338d3334f8205a8b5c856569f67f0c5e66b3034
yashwanth_devara <yashwanthbevara0@gmail.com>
2025-06-12 12:37:49 +0530

    intial_commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,26 +2,36 @@ import { Component, input } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
-
+ 
 @Component({
   selector: 'app-root',
   imports: [CommonModule, FormsModule, RouterModule],
   templateUrl: './app.component.html',
 styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = "FirstProgram";
-  email: string = "";
-  subject: string = "";
-  scoreOne: number = 0; 
-  scoreSecond: number = 0; 
-  statement: boolean = false;
 
-  onclick(value: string, value2: string, value3: number, value4: number) {
-    this.email = value;
-    this.subject = value2;
-    this.scoreOne = value3;
-    this.scoreSecond = value4;
-    this.statement= !this.statement  // <-- Add this line 
+export class AppComponent {
+  title = 'firstprogram';
+  students: { name: string; grade: string }[] = [];
+
+  studentName: string = '';
+  studentGrade: string = '';
+
+  addStudent(nameInput: HTMLInputElement, gradeInput: HTMLInputElement) {
+    if (this.studentName && this.studentGrade) {
+      this.students.push({
+        name: this.studentName,
+        grade: this.studentGrade,
+      });
+
+      // Clear inputs using template reference variables
+      nameInput.value = '';
+      gradeInput.value = '';
+
+      // Reset model values
+      this.studentName = '';
+      this.studentGrade = '';
+    }
   }
 }
+
